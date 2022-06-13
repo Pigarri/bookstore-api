34eccd5016e11358a7778e9cf3d4b26c324c8d8b
unknown <admin@NT001111.spassu.com.br>
2022-06-13 15:45:40 -0300

    tratando exeções

--- src/main/java/com/diegopigarri/bookstore/service/CategoriaService.java
+++ src/main/java/com/diegopigarri/bookstore/service/CategoriaService.java
@@ -7,17 +7,19 @@ import org.springframework.stereotype.Service;
 
 import com.diegopigarri.bookstore.domain.Categoria;
 import com.diegopigarri.bookstore.ropository.CategoriaRepository;
+import com.diegopigarri.bookstore.service.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
 	}
 
 }
