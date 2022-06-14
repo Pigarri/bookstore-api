156cc25d84b3f2d6c5f9961a7f48216fbbc2c3d8
Diego.Pigarri <diego.pigarri@gmail.com>
2022-06-14 08:32:57 -0300

    Implementação do verbo POST para Categoria

--- src/main/java/com/diegopigarri/bookstore/rosurce/CategoriaResource.java
+++ src/main/java/com/diegopigarri/bookstore/rosurce/CategoriaResource.java
@@ -1,14 +1,20 @@
 package com.diegopigarri.bookstore.rosurce;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.servlet.ServletSecurityElement;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.diegopigarri.bookstore.domain.Categoria;
 import com.diegopigarri.bookstore.dto.CategoriaDTO;
@@ -36,5 +42,13 @@ public class CategoriaResource {
 
 		return ResponseEntity.ok().body(listDTO);
 
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+		
 	}
 }
